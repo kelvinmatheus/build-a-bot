fa7b4db54c8f818da1de40d613bf770546a67c8f
Kelvin Firmino <kelvinmatheus@gmail.com>
2018-07-09 16:03:29 -0400

    3.13 - Conditionally Applying Styles with Style Bindings

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part" :style="headBorderStyle">
+      <div :class="[saleBorderClass, 'top', 'part']">
         <div class="robot-name">
           {{selectedRobot.head.title}}
           <span v-show="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -85,6 +85,9 @@
       };
     },
     computed: {
+      saleBorderClass() {
+        return this.selectedRobot.head.onSale ? 'sale-border' : '';
+      },
       headBorderStyle() {
         return {
           border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid #aaa'
@@ -272,5 +275,9 @@
 
   .cost {
     text-align: right;
+  }
+
+  .sale-border {
+    border: 3px solid red;
   }
 </style>
