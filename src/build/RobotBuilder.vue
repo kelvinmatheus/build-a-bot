670c7c2d2f691e3f566b8be6045c009d9960ee49
Kelvin Firmino <kelvinmatheus@gmail.com>
2018-07-09 14:06:10 -0400

    3.8 - Displaying Data with Interpolation

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="top-row">
       <div class="top part">
+        <div class="robot-name">{{selectedRobot.head.title}}</div>
         <img :src="selectedRobot.head.src" title="head"/>
         <button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
         <button @click="selectNextHead()" class="next-selector">&#9658;</button>
@@ -199,5 +200,12 @@
   }
   .right .next-selector {
     right: -3px;
+  }
+
+  .robot-name {
+    position: absolute;
+    top: -25px;
+    text-align: center;
+    width: 100%;
   }
 </style>
