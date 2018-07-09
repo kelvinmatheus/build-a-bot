4f2c8269d2566f01b3078c1bc75fa2d92842f157
Kelvin Firmino <kelvinmatheus@gmail.com>
2018-07-09 14:58:34 -0400

    3.10 - Repeating Elements with v-for

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="content">
+    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
       <div class="top part">
         <div class="robot-name">
@@ -35,6 +36,24 @@
         <button @click="selectNextBase()" class="next-selector">&#9658;</button>
       </div>
     </div>
+
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -57,6 +76,7 @@
     data() {
       return {
         availableParts,
+        cart: [],
         selectedHeadIndex: 0,
         selectedLeftArmIndex: 0,
         selectedTorsoIndex: 0,
@@ -76,11 +96,22 @@
       }
     },
     methods: {
+      addToCart() {
+        const robot = this.selectedRobot;
+        const cost =
+          robot.head.cost +
+          robot.leftArm.cost +
+          robot.torso.cost +
+          robot.rightArm.cost +
+          robot.base.cost;
+        this.cart.push(Object.assign({}, robot, { cost }));
+      },
+
       selectNextHead() {
         this.selectedHeadIndex = getNextValidIndex(this.selectedHeadIndex, availableParts.heads.length);
       },
       selectPreviousHead() {
-        this.selectedHeadIndex = getPrevieousValidIndex(this.selectedHeadIndex, availableParts.heads.length);
+        this.selecxtedHeadIndex = getPrevieousValidIndex(this.selectedHeadIndex, availableParts.heads.length);
       },
 
       selectNextLeftArm() {
@@ -214,5 +245,27 @@
 
   .sale {
     color: red;
+  }
+
+  .content {
+    position: relative;
+  }
+
+  .add-to-cart {
+    position: absolute;
+    right: 30px;
+    width: 220px;
+    padding: 3px;
+    font-size: 16px;
+  }
+
+  td,th {
+    text-align: left;
+    padding: 5px;
+    padding-right: 20px;
+  }
+
+  .cost {
+    text-align: right;
   }
 </style>
