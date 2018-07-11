d88177cd8276f4b731831c2483c2828cfc57bac3
Kelvin Firmino <kelvinmatheus@gmail.com>
2018-07-11 02:21:38 -0400

    5.9 - Using Named Views

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,14 @@
         </ul>
       </nav>
     </header>
-    <main>
-      <router-view />
-    </main>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar"/>
+      </aside>
+      <main>
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
@@ -46,16 +51,15 @@
   }
 
   main {
-    margin: 0 auto;
     padding: 30px;
     background-color: white;
-    width: 1024px;
+    width: 964px;
     min-height: 300px;
   }
 
   header {
     background-color: #999;
-    width: 1084px;
+    width: 1184px;
     margin: 0 auto;
   }
   ul {
@@ -80,5 +84,18 @@
 
   .router-link-active {
     color: white;
+  }
+
+  .container {
+    display: flex;
+    margin: 10px auto 0 auto;
+    justify-content: center;
+  }
+
+  .aside {
+    padding: 30px;
+    background-color: #aaa;
+    width: 100px;
+    min-height: 300px;
   }
 </style>
