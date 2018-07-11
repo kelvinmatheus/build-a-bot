7a30186d6c2101b964642144efb865c23c925427
Kelvin Firmino <kelvinmatheus@gmail.com>
2018-07-11 13:10:23 -0400

    6.5 - Using Vuex Getters to Return Calculated Data

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -23,6 +23,30 @@
       </tr>
       </tbody>
     </table>
+
+    <h2 class="sale-items">You saved money on these robots: </h2>
+    <table>
+      <thead>
+      <tr>
+        <th class="robot-title">
+          Robot
+        </th>
+        <th class="cost">
+          Cost
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(robot, index) in cartSaleItems" :key="index">
+        <td class="robot-title">
+          {{robot.head.title}}
+        </td>
+        <td class="cost">
+          {{robot.cost}}
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -32,6 +56,9 @@
     computed: {
       cart() {
         return this.$store.state.cart;
+      },
+      cartSaleItems() {
+        return this.$store.getters.cartSaleItems;
       }
     }
   };
@@ -47,5 +74,11 @@
   }
   .cost {
     text-align: right;
+  }
+
+  .sale-items {
+    margin-top: 50px;
+    font-size: 18px;
+    color: red;
   }
 </style>
