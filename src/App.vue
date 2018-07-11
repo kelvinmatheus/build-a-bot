7c2825d7fd6a6330822bdea67daedb113bc65a74
Kelvin Firmino <kelvinmatheus@gmail.com>
2018-07-11 12:02:30 -0400

    6.4 - Retrieving Items from the Vuex Store

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,15 @@
               Build
             </router-link>
           </li>
+
+          <li class="nav-item cart">
+            <router-link class="nav-link" to="/cart" exact>
+              Cart
+            </router-link>
+            <div class="cart-items">
+              {{cart.length}}
+            </div>
+          </li>
         </ul>
       </nav>
     </header>
@@ -32,7 +41,12 @@
 <script>
 
   export default {
-    name: 'app'
+    name: 'app',
+    computed: {
+      cart() {
+        return this.$store.state.cart;
+      }
+    }
   };
 
 </script>
@@ -72,6 +86,13 @@
     font-size: 22px;
     border-right: 1px solid #bbb;
   }
+
+  .nav-item.cart {
+    position: relative;
+    margin-left: auto;
+    border-right: none;
+  }
+
   .logo {
     vertical-align: middle;
     height: 30px;
@@ -97,5 +118,17 @@
     background-color: #aaa;
     width: 100px;
     min-height: 300px;
+  }
+
+  .cart-items {
+    position: absolute;
+    top: -5px;
+    right: -9px;
+    font-size: 18px;
+    width: 20px;
+    text-align: center;
+    display: inline-block;
+    border-radius: 100px;
+    background-color: mediumseagreen;
   }
 </style>
